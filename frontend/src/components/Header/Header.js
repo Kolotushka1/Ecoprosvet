90bdf023d65b505172336c0ad6cd16eee878e1a3
Saveliy Kochanov <kochanov.saveliy222@mail.ru>
2024-09-18 13:31:12 +0700

    init

---
+++ frontend/src/components/Header/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export const Header = () => {
+	return (
+		<header className='header'>
+			<nav className='header__nav'>
+				<ul className='header__nav-list'>
+					<li className='header__nav-item'>
+						<a className='header__nav-link'>Хуй</a>
+					</li>
+					<li className='header__nav-item'>
+						<a className='header__nav-link'>пизда</a>
+					</li>
+					<li className='header__nav-item'>
+						<a className='header__nav-link'></a>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	)
+}
